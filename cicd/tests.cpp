eed65770eba89156f0ddc508178e9f3040b4a03d
Simon Duslov <susloff-d2014@yandex.ru>
2025-03-15 03:08:03 +0300

    фикс tests.cpp

--- cicd/tests.cpp
+++ cicd/tests.cpp
@@ -1,9 +1,9 @@
 #define CATCH_CONFIG_MAIN
-#if __has_include(<catch2/catch_all.hpp>)
+//#if __has_include(<catch2/catch_all.hpp>)
     #include <catch2/catch_all.hpp>
-#else
-    #include <catch2/catch.hpp>
-#endif
+// #else
+//     #include <catch2/catch.hpp>
+// #endif
 #include "../src/isPrime.h"
 
 
